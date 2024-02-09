e91508b9d964bf642a98f04e0d365a918a257112
Kamil Ozdemir <ozdemirkamil@gmail.com>
2024-02-09 11:36:58 +0100

    25. gün sonu

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@resim",
+        tags = "@wip",
         dryRun = false // true oldugunda sadece eksik adim var mi diye kontrol eder
 )
 public class Runner {
